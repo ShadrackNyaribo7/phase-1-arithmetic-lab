52e8ab4433722876d852e4a258e1e6dc6b9144f0
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-25 11:17:16 -0800

    added

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+num1 = 31;
+num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.random();
+num3 = 16;
+num4 = 4;
+
+let mod = num3 % num4;
+
+let max = Math.max(10,12,20);
